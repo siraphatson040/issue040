091bdc4d623e5caa1aae2d7d359bcbe6700ffad5
siraphatson040 <siraphatson.s@rmutsvmail.com>
2025-02-05 09:22:40 +0700

    เเก้ไขไฟล์ contact.php

--- contact.php
+++ contact.php
@@ -14,7 +14,7 @@
             </td>
             <td width="190" bgcolor="#FFFF66"><ul>
 				<li><a href="home.php">หน้าแรก</li>
-				<li><a href="room.php">ห้องพัก&ราคา</li>
+				<li><a href="room.php">ห้องพัก&ราคา</li>5455432
 				<li><a herf="guest.php">สมุดเยี่ยม</li>
 				<li><a herf="contact.php">ติดต่อ</li>
                 <li><a herf="detail.php">รายละเอียดเพิ่มเติม12345</li>
